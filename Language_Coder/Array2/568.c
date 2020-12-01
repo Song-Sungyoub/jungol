2b62e60a0da6b90cf7c594007be441df5862737b
Song-Sungyoub <ssyoub0327@daum.net>
2020-12-02 03:00:05 +0900

    jungol array2 568 clear

--- Language_Coder/Array2/568.c
+++ Language_Coder/Array2/568.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 
 int main(){
-	
+	int f[2][4], s[2][4];
+
 	printf("first array\n");
 	for(int i = 0; i<2;i++){
 		for(int j = 0; j<4;j++){
-			
+			scanf("%d", &f[i][j]);
 		}
+	}
+	printf("second array\n");
+	for(int i  = 0;i<2;i++){
+		for(int j = 0; j<4;j++){
+			scanf("%d", &s[i][j]);
+		}
+	}
+	for(int i = 0;i<2;i++){
+		for(int j = 0;j<4;j++){
+			printf("%d ", f[i][j]*s[i][j]);
+		}
+		printf("\n");
 	}
 }
