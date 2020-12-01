54729917fc336ac1da72a9bb27b2a96e21f2ede1
Song-Sungyoub <ssyoub0327@daum.net>
2020-12-02 03:28:46 +0900

    정올 배열2 570 문제 해결

---
+++ Language_Coder/Array2/570.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	int n[5][5];
+
+	for(int i = 0 ;i<5;i++){
+		n[i][0] = 1;
+		n[0][i] = 1;
+	}
+	
+	for(int i = 1;i<5;i++){
+		for(int j = 1;j<5;j++){
+			n[i][j] = n[i][j-1] + n[i-1][j];
+		}
+	}
+	for(int i = 0;i<5;i++){
+		for(int j = 0;j<5;j++){
+			printf("%d ", n[i][j]);
+		}
+		printf("\n");
+	}
+}
