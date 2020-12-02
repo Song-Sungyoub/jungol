0b961e09a71911ef942b7c91e4635a62f2bf5939
Song-Sungyoub <ssyoub0327@daum.net>
2020-12-02 17:02:40 +0900

    정올 배열2 166.c

--- Language_Coder/Array2/166.c
+++ Language_Coder/Array2/166.c
@@ -1,1 +1,21 @@
-#include 
+#include <stdio.h>
+
+int main(){
+	int array[2][2][3];
+	char *count[2] = {"first", "second"};
+
+	for(int n = 0;n<2;n++){
+		printf("%s array\n", count[n]);
+		for(int i = 0;i<2;i++){
+			for(int j = 0;j<3;j++){
+				scanf("%d", &array[n][i][j]);
+			}
+		}
+	}
+	for(int i=0;i<2;i++){
+		for(int j = 0;j<3;j++){
+			printf("%d ", array[0][i][j] * array[1][i][j]);
+		}
+		printf("\n");
+	}
+}
