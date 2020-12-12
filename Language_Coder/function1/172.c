5e0a799396753df3d62821cea6873f79eab96fb4
Song-Sungyoub <ssyoub0327@daum.net>
2020-12-12 09:27:39 +0900

    re 172.c

--- Language_Coder/function1/172.c
+++ Language_Coder/function1/172.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
-int main(){
-	int input;
-	scanf("%d", &input);
-	int array[input][input];
+void makeRect(int n){
+	int array[n][n];
 
-	for(int i=0;i<input;i++){
-		for(int j=0;j<input;j++){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
 			array[i][j] = (i+1)*(j+1);
 			printf("%d ", array[i][j]);
 		}
 		printf("\n");
 	}
+}
+
+int main(){
+	int input;
+	scanf("%d", &input);
+	
+	makeRect(input);
 }
